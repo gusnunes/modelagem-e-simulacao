38e9d8a889f7e9410fe18717b1e9ec08083a8520
gusnunes <daniloaugusto_nunes@hotmail.com>
2021-05-09 01:23:00 -0300

    função que lê tempos do .csv

--- trab1/mm1.py
+++ trab1/mm1.py
@@ -4,8 +4,19 @@ import numpy as np
 import csv
 from random import random
 
+# Lê os dados da tabela de tempos (entre chegadas e serviços)
+def le_dados(nome_arquivo):
+  with open(nome_arquivo, 'r') as csvfile:
+    valores = []
+    
+    for linha in csv.reader(csvfile, delimiter=','):
+      valores.extend(map(float,linha))
+  
+  return valores
+
 def main():
-  pass
+  tec = le_dados("TEC.csv")
+  print(tec)
 
 if __name__ == "__main__":
   main()
