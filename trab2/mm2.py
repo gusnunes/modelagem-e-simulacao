686efc1074d2994a542c275ee288fe3fd91ecd34
gusnunes <daniloaugusto_nunes@hotmail.com>
2021-06-14 16:22:43 -0300

    limite de fila implementado

--- trab2/mm2.py
+++ trab2/mm2.py
@@ -28,13 +28,14 @@ def gera_tempos(distribuicao,qtd_valores):
 
     return valores
 
-def realiza_simulacao(qtd_atendentes, tec_distr, ts_distr):
+def realiza_simulacao(qtd_atendentes, tec_distr, ts_distr, limite_fila):
     # limite da execução da simulação
-    chegadas = 10
+    limite_simulacao = 10
+    chegadas = 0
 
     # tempos entre chegadas e tempos de serviço
-    tec = gera_tempos(tec_distr,chegadas)
-    ts = gera_tempos(ts_distr,chegadas)
+    tec = gera_tempos(tec_distr,limite_simulacao)
+    ts = gera_tempos(ts_distr,limite_simulacao)
 
     TR = TEC = TF = 0
     fila = []
@@ -46,7 +47,13 @@ def realiza_simulacao(qtd_atendentes, tec_distr, ts_distr):
     # quantidade de atendentes do sistema
     atendentes = [Atendente(i+1) for i in range(qtd_atendentes)]
 
-    for i in range(chegadas):
+    while chegadas < limite_simulacao:
+        fila_cheia = False
+        
+        # Precisa gerar mais valores
+        if len(tec) == 0:
+            tec = gera_tempos(tec_distr,limite_simulacao)
+
         for atendente in atendentes:
             # atendente está desocupado
             if TR >= atendente.TS_F:
@@ -56,28 +63,38 @@ def realiza_simulacao(qtd_atendentes, tec_distr, ts_distr):
         
         # todos os atendentes estão ocupados
         else:
-            # qual atendente ficará disponível primeiro
-            atendente_disponivel = min(atendentes, key=lambda x: x.TS_F)
-            atendente_disponivel.TS_I = atendente_disponivel.TS_F
-            
             # controlar quantidade de entidades presentes na fila
             for entidade in fila:
                 tempo_inicio = entidade   # quando a entidade da fila vai ser atendida
                 if TR >= tempo_inicio:
                     fila.remove(tempo_inicio) 
 
-            fila.append(atendente_disponivel.TS_I)
+            tamanho_fila = len(fila)
             
-            #print(fila)
+            # não alcançou o limite da fila
+            if tamanho_fila < limite_fila:
+                # qual atendente ficará disponível primeiro
+                atendente_disponivel = min(atendentes, key=lambda x: x.TS_F)
+                atendente_disponivel.TS_I = atendente_disponivel.TS_F
+                
+                fila.append(atendente_disponivel.TS_I)
+            
+            else:
+                fila_cheia = True
+            
+        # cliente pode ser atendido
+        if not fila_cheia:
+            atendente_disponivel.TS = ts.pop()
+            atendente_disponivel.TS_F = atendente_disponivel.TS_I + atendente_disponivel.TS
 
-        atendente_disponivel.TS = ts.pop()
-        atendente_disponivel.TS_F = atendente_disponivel.TS_I + atendente_disponivel.TS
+            TF = atendente_disponivel.TS_I - TR
 
-        TF = atendente_disponivel.TS_I - TR
-
-        simulacao.loc[i] = [round(i+1),TEC,TR,atendente_disponivel.nome,atendente_disponivel.TS_I, 
-            atendente_disponivel.TS,atendente_disponivel.TS_F,TF] 
+            simulacao.loc[chegadas] = [chegadas+1,TEC,TR,atendente_disponivel.nome,atendente_disponivel.TS_I, 
+                atendente_disponivel.TS,atendente_disponivel.TS_F,TF]
+            
+            chegadas += 1
         
+        # marca a próxima chegada
         TEC = tec.pop()
         TR += TEC
     
@@ -92,13 +109,13 @@ def main():
     ts_distr = {}
     
     # menu que apresenta as opções
-    tec_nome,tec_valor,ts_nome,ts_valor = menu()
+    tec_nome,tec_valor,ts_nome,ts_valor,limite_fila = menu()
 
     # distribuição do tec e ts
     tec_distr[tec_nome] = tec_valor
     ts_distr[ts_nome] = ts_valor
     
-    resultado_simulacao = realiza_simulacao(atendentes,tec_distr,ts_distr)
+    resultado_simulacao = realiza_simulacao(atendentes,tec_distr,ts_distr,limite_fila)
     resultado_simulacao[["Cliente","Atendente"]] = resultado_simulacao[["Cliente","Atendente"]].astype(int)
     print(resultado_simulacao.to_string(index=False))
 
