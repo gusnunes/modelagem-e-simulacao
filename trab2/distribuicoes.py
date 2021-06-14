e961a5870a7870cd5595239cb18ca9cb50c7ede8
gusnunes <daniloaugusto_nunes@hotmail.com>
2021-06-13 23:37:24 -0300

    arredondando os valores

--- trab2/distribuicoes.py
+++ trab2/distribuicoes.py
@@ -2,12 +2,14 @@ from random import random
 from math import log,cos,pi,sqrt
 
 # método da transformação inversa
-def distribuicao_exponencial(lambda_):
+def distribuicao_exponencial(media):
+    lambda_ = 1/media
+    
     # valor de uma distribuição uniforme (0,1)
     u = random()   
     
     x = (-1/lambda_)*log(1-u)
-    return x
+    return round(x,2)
 
 # método Box-Muller
 def distribuicao_normal(media, desvio_padrao):
@@ -19,4 +21,4 @@ def distribuicao_normal(media, desvio_padrao):
     z = cos(2*pi*u2)*sqrt(-2*log(u1))
     
     x = (desvio_padrao*z) + media
-    return x
+    return round(x,2)
